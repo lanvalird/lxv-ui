7d86805bb0c0774ce882eb2acaa84ad45e167857
lanvalird <strazhognay@gmail.com>
2026-05-11 01:59:58 +0400

    feat(docs): add stories for main and subs item components

---
+++ src/indev/primitives/item/stories/item-description.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { ItemDescription } from "../item-description";
+
+const meta: Meta<typeof ItemDescription> = {
+  title: "Components/ItemDescription",
+  component: ItemDescription,
+  parameters: { layout: "centered" },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof ItemDescription>;
+
+export const Default: Story = {
+  args: {
+    children:
+      "This is a standard description that will be clamped after two lines if it becomes too long. It supports links like <a href='#'>this</a>.",
+  },
+};
+
+export const WithLink: Story = {
+  args: {
+    children: (
+      <>
+        Learn more about our <a href="#">privacy policy</a> and{" "}
+        <a href="#">terms of service</a>.
+      </>
+    ),
+  },
+};
